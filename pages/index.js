fec5cdc778dde1d0c32af9b061d62462650e7d21
Renan Abreu <renan.breu04@gmail.com>
2026-09-15 14:17:06 -0300

    desafio

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+function Home() {
+  return (
+    <div>
+      <h1>Te amo chatonilda 🧡</h1>
+      <img 
+        src="/teste.jpg" 
+        alt="Minha imagem"
+        width="500"
+      />
+    </div>
+  )
+}
+
+export default Home;
